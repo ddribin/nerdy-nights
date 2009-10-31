84679af8853b0437b9b3914dae50929d536eac8b
Dave Dribin <dave@dribin.org>
2009-10-31 00:08:13 -0500

    Use SMB1 sprite palette

--- sprites/sprites.asm
+++ sprites/sprites.asm
@@ -94,7 +94,7 @@ palette:
 	.byte	$0F,$39,$3A,$3B
 	.byte	$0F,$3D,$3E,$0F
 	;; Sprite palette
-	.byte 	$0F,$1C,$15,$14
+	.byte 	$0F,$16,$27,$18
 	.byte	$0F,$02,$38,$3C
 	.byte	$0F,$1C,$15,$14
 	.byte	$0F,$02,$38,$3C
