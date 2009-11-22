d1772cf66f7734d4024f2879da3a4b42941a238c
Dave Dribin <dave@dribin.org>
2009-11-21 22:39:51 -0600

    Add sound/skeleton

---
+++ sound/skeleton/sound_engine.h
@@ -0,0 +1,12 @@
+
+	.import		sound_init
+	.import 	sound_disable
+	.import		sound_load
+	.import 	sound_play_frame
+
+	.importzp 	sound_disable_flag
+	.importzp	sfx_playing
+
+; Local Variables: 
+; mode: asm
+; End: 
