c74a49567fc8d816294c920000d2baa55082379f
Dave Dribin <dave@dribin.org>
2009-11-21 22:51:58 -0600

    Updates to comments

--- sound/periods/periods.asm
+++ sound/periods/periods.asm
@@ -5,25 +5,25 @@
 	.byte	"NES", $1A	; iNES header identifier
 	.byte	2		; 2x 16KB PRG code
 	.byte	1		; 1x  8KB CHR data
-	.byte	$01, $00	; mapper 0, vertical mirroring
+	.byte	$01, $00	; Mapper 0, vertical mirroring
 
 ;;;;;;;;;;;;;;;
 
 .zeropage
 
-joypad1:		.res	1	; button states for the current frame
-joypad1_old:		.res 	1	; last frame's button states
-joypad1_pressed:	.res	1	; current frame's off_to_on transitions
-current_note:		.res	1	; used to index into our note_table
-note_value:		.res	1	; there are 12 possible note values.
+joypad1:		.res	1	; Button states for the current frame
+joypad1_old:		.res 	1	; Last frame's button states
+joypad1_pressed:	.res	1	; Current frame's off_to_on transitions
+current_note:		.res	1	; Used to index into our note_table
+note_value:		.res	1	; There are 12 possible note values.
 					; (A-G#, represented by $00 - $0B)
-note_octave:		.res	1	; what octave our note is in (1-9)
+note_octave:		.res	1	; What octave our note is in (1-9)
 	
-;;; main program sets this and waits for the NMI to clear it.  Ensures the main
+;;; Main program sets this and waits for the NMI to clear it.  Ensures the main
 ;;; program is run only once per frame.  For more information, See Disch's document.
 sleeping:		.res	1
 
-ptr1:			.res	1 	; a pointer
+ptr1:			.res	2 	; An address pointer
 
 ;;;;;;;;;;;;;;;
 	
@@ -33,13 +33,13 @@ ptr1:			.res	1 	; a pointer
 .segment "CODE"
 
 reset:
-	sei			; disable IRQs
-	cld			; disable decimal mode
+	sei			; Disable IRQs
+	cld			; Disable decimal mode
 	ldx	#$ff		; Set up stack
 	txs			;  .
-	inx			; now X = 0
+	inx			; Now X = 0
 
-	;; first wait for vblank to make sure PPU is ready
+	;; First wait for vblank to make sure PPU is ready
 vblankwait1:
 	bit	$2002
 	bpl	vblankwait1
@@ -54,11 +54,11 @@ clear_memory:
 	sta	$0600, x
 	sta	$0700, x
 	lda	#$fe
-	sta	$0200, x	; move all sprites off screen
+	sta	$0200, x	; Move all sprites off screen
 	inx
 	bne	clear_memory
 
-	;; second wait for vblank, PPU is ready after this
+	;; Second wait for vblank, PPU is ready after this
 vblankwait2:
 	bit	$2002
 	bpl	vblankwait2
@@ -82,62 +82,62 @@ clear_nametables:
 	
 	;; Set a couple palette colors. This demo only uses two.
 init_palette:
-	lda	$2002		; reset PPU HI/LO latch
+	lda	$2002		; Reset PPU HI/LO latch
 	
 	lda	#$3f
 	sta	$2006
 	lda	#$00
-	sta	$2006		; palette data starts at $3F00
+	sta	$2006		; Palette data starts at $3F00
 
-	lda	#$0f		; black
+	lda	#$0f		; Black
 	sta	$2007
-	lda	#$30		; white
+	lda	#$30		; White
 	sta	$2007
 
 enable_sound_channels:
 	lda	#%00000001
-	sta	$4015		; enable Square 1
+	sta	$4015		; Enable Square 1
 
 	lda	#C4
-	sta	current_note	; start with a middle C
+	sta	current_note	; Start with a middle C
 	jsr	get_note_and_octave
 
 	lda	#$88
-	sta	$2000		; enable NMIs
+	sta	$2000		; Enable NMIs
 	lda	#$18
-	sta	$2001		; turn PPU on
+	sta	$2001		; Turn PPU on
 	
 forever:
 	inc	sleeping	; go to sleep (wait for NMI).
 	
-	;; wait for NMI to clear the sleeping flag and wake us up
+	;; Wait for NMI to clear the sleeping flag and wake us up
 @loop:
 	lda	sleeping
 	bne	@loop
 
-	;; when NMI wakes us up, handle input and go back to sleep
+	;; When NMI wakes us up, handle input and go back to sleep
 	jsr	read_joypad
 	jsr	handle_input
 	
 	jmp	forever
 
 nmi:
-	;; backup registers
+	;; Push all registers on the stack
 	pha
 	txa
 	pha
 	tya
 	pha
 
-	jsr	draw_note	; draws the note and octave to the screen
-	lda	#$00		; set scroll
+	jsr	draw_note	; Draws the note and octave to the screen
+	lda	#$00		; Set scroll
 	sta	$2005
 	sta	$2005
 
 	lda	#$00
-	sta	sleeping	; wake up the main program
+	sta	sleeping	; Wake up the main program
 
-	;; restore regisers
+	;; Pull all registers off the stack
 	pla
 	tay
 	pla
@@ -152,7 +152,7 @@ nmi:
 ;;; 
 read_joypad:
 	lda	joypad1
-	sta	joypad1_old	; save last frame's joypad button states
+	sta	joypad1_old	; Save last frame's joypad button states
 
 	lda	#$01
 	sta	$4016
@@ -167,10 +167,10 @@ read_joypad:
 	dex
 	bne	@loop
 
-	lda	joypad1_old	; what was pressed last frame. EOR to flip all the bits
+	lda	joypad1_old	; What was pressed last frame. EOR to flip all the bits
 	eor	#$ff		;  to find what was not pressed last frame.
-	and	joypad1		; what is pressed this frame
-	sta	joypad1_pressed	; stores off-to-on transitoins
+	and	joypad1		; What is pressed this frame
+	sta	joypad1_pressed	; Stores off-to-on transitoins
 	
 	rts
 
@@ -182,25 +182,25 @@ read_joypad:
 ;;;  right - cycle up a note
 handle_input:
 	lda	joypad1_pressed
-	and	#$0f		; check d-pad only
+	and	#$0f		; Check d-pad only
 	beq	@done
 @check_up:
-	and	#$08		; up
+	and	#$08		; Up
 	beq	@check_down
 	jsr	play_note
 @check_down:
 	lda	joypad1_pressed
-	and	#$04		; down
+	and	#$04		; Down
 	beq	@check_left
 	jsr	silence_note
 @check_left:
 	lda	joypad1_pressed
-	and	#$02		; left
+	and	#$02		; Left
 	beq	@check_right
 	jsr	note_down
 @check_right:
 	lda	joypad1_pressed
-	and	#$01		; right
+	and	#$01		; Right
 	beq	@done
 	jsr	note_up
 @done:
@@ -210,18 +210,19 @@ handle_input:
 ;;; play_note plays the note stored in the current_note
 ;;; 
 play_note:
-	lda	#$7f		; duty 01, volume F
+	lda	#$7f		; Duty 01, volume F
 	sta	$4000
-	lda	#$08		; set negate flag so low notes aren't silenced
+	lda	#$08		; Set negate flag so low notes aren't silenced
 	sta	$4001
 
 	lda	current_note
-	asl	a		; multiply by 2 because we are indexing into a word table
+	;; Multiply by 2 because we are indexing into a word table
+	asl	a
 	tay
-	lda	note_table, y	; read the low byte of the period
-	sta	$4002		; write to SQ1_LO
-	lda	note_table+1, y	; read the high byte of the period
-	sta	$4003		; write to SQ1_HI
+	lda	note_table, y	; Read the low byte of the period
+	sta	$4002		; Write to SQ1_LO
+	lda	note_table+1, y	; Read the high byte of the period
+	sta	$4003		; Write to SQ1_HI
 	rts
 
 ;;;
@@ -229,7 +230,7 @@ play_note:
 ;;; 
 silence_note:
 	lda	#$30
-	sta	$4000		; silence Square 1 by setting the volume to 0
+	sta	$4000		; Silence Square 1 by setting the volume to 0
 	rts
 
 ;;;
@@ -241,7 +242,7 @@ note_down:
 	lda	current_note
 	cmp	#$ff
 	bne	@done
-	lda	#Fs9		; highest note. We wrapped from 0.
+	lda	#Fs9		; Highest note. We wrapped from 0.
 	sta	current_note
 @done:
 	jsr	get_note_and_octave
@@ -254,7 +255,7 @@ note_down:
 note_up:
 	inc	current_note
 	lda	current_note
-	cmp	#Fs9+1		; did we move past the highest note index in our table?
+	cmp	#Fs9+1		; Did we move past the highest note index in our table?
 	bne	@done
 	sta	current_note
 @done:
@@ -269,15 +270,15 @@ get_note_and_octave:
 	ldx	#$02
 	lda	current_note
 	cmp	#$0c
-	bcc	@store_note_value ; if we are in the lowest octave already, we are done
+	bcc	@store_note_value ; If we are in the lowest octave already, we are done
 	sec			  ; else we need to find out what octave we are in.
 @loop:
-	sbc	#$0c		; subtract an octave
-	inx			; count how many subtractions we've made
-	cmp	#$0c		; when we ar down to the lowest octave, quit
+	sbc	#$0c		; Subtract an octave
+	inx			; Count how many subtractions we've made
+	cmp	#$0c		; When we ar down to the lowest octave, quit
 	bcs	@loop
 @store_note_value:
-	sta	note_value	; store the note value
+	sta	note_value	; Store the note value
 	cmp	#$03
 	bcs	@store_octave
 	dex			; On the NES, A, A# and B start at octave 1, not 2.
@@ -296,30 +297,32 @@ draw_note:
 	lda	#$4d
 	sta	$2006		; $214D is a nice place in the middle of the screen
 
-	lda	note_value	; use note_value as an index into our pointer table
-	asl	a		; multiply by 2 because we are indexing into a word table
+	lda	note_value	; Use note_value as an index into our pointer table
+	;; multiply by 2 because we are indexing into a word table
+	asl	a
 	tay
-	lda	text_pointers, y ; setup pointer to the text data
+	lda	text_pointers, y ; Setup pointer to the text data
 	sta	ptr1
 	lda	text_pointers+1, y
 	sta	ptr1+1
 	ldy	#$00
 @loop:
 	lda	(ptr1), y	; read a byte from the string
-	bmi	@end		; if negative, we are finished
-				;  (our strings are terminaged by $FF, a negative number)
-	sta	$2007		; else draw on the screen
+	;; If negative, we are finished (our strings are terminated by $FF, a
+	;; negative number)
+	bmi	@end
+	sta	$2007		; Else draw on the screen
 	iny
 	jmp	@loop
 @end:
-	lda	note_octave	; the CHR #s for the numbers are $01-$09, so we can
+	lda	note_octave	; The CHR #s for the numbers are $01-$09, so we can
 				;  just write the value directly.
 	sta	$2007
 	rts
 
 .segment "RODATA"
 
-;;; this is a table of poitners. These pointers point to the beginning of text strings
+;;; This is a table of poitners. These pointers point to the beginning of text strings
 text_pointers:
 	.word	text_A, text_Asharp, text_B, text_C, text_Csharp, text_D
 	.word	text_Dsharp, text_E, text_F, text_Fsharp, text_G, text_Gsharp
@@ -343,7 +346,7 @@ text_Fsharp:	.byte	$15, $0a, $ff
 text_G:		.byte	$00, $16, $ff
 text_Gsharp:	.byte	$16, $0a, $ff
 .else
-	;; Alternate method using CA65's character maps
+	;; Alternate implementation using CA65's character maps
 	.charmap	' ', $00
 	.charmap	'#', $0a
 	.charmap	'A', $10
