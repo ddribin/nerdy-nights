1c076378b53d157012e851daecd1c24fd0c426d4
Dave Dribin <dave@dribin.org>
2009-11-22 23:05:32 -0600

    Add song 2 and 3

--- sound/headers/sound_engine.asm
+++ sound/headers/sound_engine.asm
@@ -252,6 +252,10 @@ se_set_apu:
 ;;; This is our poitner table. Each entry is a pointer to a song header
 	.import song0_header
 	.import song1_header
+	.import song2_header
+	.import song3_header
 song_headers:
 	.word	song0_header	; This is a silence song.
 	.word	song1_header	; Evil, demented notes
+	.word	song2_header	; A sound effect. Try playing it over other songs
+	.word	song3_header	; A little chord progression
