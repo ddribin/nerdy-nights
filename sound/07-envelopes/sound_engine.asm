1f9376da8204526dd909c95e57f4b51afcfe6b28
Dave Dribin <dave@dribin.org>
2009-12-01 19:55:28 -0600

    Move song_headers into a separate .asm file from sound_engine

--- sound/07-envelopes/sound_engine.asm
+++ sound/07-envelopes/sound_engine.asm
@@ -2,6 +2,7 @@
 	.include "note_table.h"
 	.include "sound_defs.h"
 
+	.import	song_headers
 	.import	volume_envelopes
 
 	;; Exported symbols
@@ -442,19 +443,3 @@ se_set_apu:
 	lda	soft_apu_ports+15
 	sta	$400f
 	rts
-
-;;; This is our pointer table. Each entry is a pointer to a song header
-	.import song0_header
-	.import song1_header
-	.import song2_header
-	.import song3_header
-	.import	song4_header
-	.import	song5_header
-	
-song_headers:
-	.word	song0_header	; This is a silence song.
-	.word	song1_header	; Evil, demented notes
-	.word	song2_header	; A sound effect. Try playing it over other songs
-	.word	song3_header	; A little chord progression
-	.word	song4_header
-	.word	song5_header
